2f335f5e522df024e403af53aa43a0414f1a0997
rahul <rahulkumar9199270356@gmail.com>
2024-08-17 20:26:26 +0530

    init

---
+++ adult.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int age=20;
+    cin>> age;
+    id(age>=18){
+        cout <<"you are an adult";
+    }
+    else {
+        cout <<"you are not an adult"
+    }
+    retrun 0;
+}
